48348c12b8ade2667ce512bb03dea768280b1567
connorjackwood <connorjackwood@gmail.com>
2015-10-16 12:14:24 +0000

    your message goes here

--- stringsClassWork.py
+++ stringsClassWork.py
@@ -1,4 +1,4 @@
-#Strings!
+"""#Strings!
 
 #Question1
 name = input ("What is your name? ")
@@ -14,7 +14,7 @@ print("Hello",name +". Your name has",nameLength," characters and your initial i
 phone = input("Please enter your mobile number or home telephone number: ")
 phoneFront = "+44"
 phoneNumber = phone[1:11]
-print(phoneFront + phoneNumber)
+print(phoneFront + phoneNumber)"""
 
 #Question4
 dob = input("Please enter your date of birth in the format 'DD/MM/YYYY': ")
@@ -26,12 +26,24 @@ ending1 = "st"
 ending2 = "rd"
 ending3 = "th"
 ending4 = "nd"
-if dobDay == "01" or "21" or "31":
-    dayDisplay = "You were born on the",dobDay + ending1
-elif dobDay == "02" or "22":
-    dayDisplay = "You were born on the",dobDay + ending4
-elif dobDay == "03" or "23":
-    dayDisplay = "You were born on the",dobDay + ending3
+if dobDay == "01" or dobDay == "21" or dobDay == "31":
+    dayDisplay = "You were born on the " + dobDay + ending1
+elif dobDay == "02" or dobDay =="22":
+    dayDisplay = "You were born on the " + dobDay + ending4
+elif dobDay == "03" or dobDay == "23":
+    dayDisplay = "You were born on the " + dobDay + ending2
 else:
-    dayDisplay = "You were born on the",dobDay + ending2
-print("hello")
+    dayDisplay = "You were born on the " + dobDay + ending3
+
+if dobMonth == "01":
+    monthDisplay = "of the " + dobMonth+ending1 + " month" 
+elif dobMonth == "02":
+    monthDisplay = "of the " + dobMonth+ending4 + " month" 
+elif dobMonth == "03":
+    monthDisplay = "of the " + dobMonth+ending2 + " month"
+else:
+    monthDisplay = "of the " + dobMonth+ending3 +  " month"
+
+yearDisplay = "in " + dobYear
+
+print (dayDisplay,monthDisplay,yearDisplay + ".")
